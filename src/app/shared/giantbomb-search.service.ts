4d3be081483986e8a41a16e9859aa221629b426c
Christian Katzorke <c.katzorke@web.de>
2016-10-10 18:50:29 +0200

    result also shows link

--- src/app/shared/giantbomb-search.service.ts
+++ src/app/shared/giantbomb-search.service.ts
@@ -19,6 +19,8 @@ export class GiantbombSearchService {
       let search = new URLSearchParams();
       search.set('api_key', 'e316aff9ff7d945077dabd3a13ecebaad12bb70d');
       search.set('query', term);
+      search.set('field_list', 'name,site_detail_url');
+      search.set('resources', 'game');
       search.set('format', 'jsonp');
       return this.jsonp.get('http://www.giantbomb.com/api/search/?json_callback=JSONP_CALLBACK',
         {search}).map(response => {
